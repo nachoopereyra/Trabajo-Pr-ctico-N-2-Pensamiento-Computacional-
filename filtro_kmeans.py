b2931dace9b08c019591dee86c27e0582edf6c9f
nachoopereyra <ipereyra@udesa.edu.ar>
2025-05-09 11:22:51 -0300

    Filtro kmeans

--- filtro_kmeans.py
+++ filtro_kmeans.py
@@ -3,7 +3,7 @@ import numpy as np
 import random
 from funciones import *
 
-imagen = Image.open("../Trabajo-Pr-ctico-N-2-Pensamiento-Computacional-/test_images/sunrise.bmp")
+imagen = Image.open("Trabajo-Pr-ctico-N-2-Pensamiento-Computacional-/test_images/sunrise.bmp")
 canal_rojo, canal_verde, canal_azul = dividir_canales(imagen)
 w, h = imagen.size
 
@@ -22,8 +22,8 @@ for coordenada in range(k):
     r = datos_canal_rojo[y, x]
     g = datos_canal_verde[y, x]
     b = datos_canal_azul[y, x]
-    valores_pixeles_principales.append((r, g, b))
-    clusters.append(x,y)
+    valores_pixeles_principales.append([r, g, b])
+    clusters.append([x,y])
 
 #Recorro todos los píxeles y los pongo en cada cluster segun a cual centroide se asemeja más
 for i in range(h):
@@ -35,8 +35,9 @@ for i in range(h):
         for (rojo2, verde2, azul2) in valores_pixeles_principales:
             valor_punto = math.sqrt(((int(rojo) - int(rojo2)) ** 2) + ((int(verde) - int(verde2)) ** 2) + ((int(azul) - int(azul2)) ** 2))
             lista_valores.append(valor_punto)
-        cluster_mas_cercano = lista_valores.index(min(lista_valores))
-        for i in range(len(clusters)):
+        indice_minimo = lista_valores.index(min(lista_valores))
+        clusters[indice_minimo].append([i, j])
+        
     
 
             
